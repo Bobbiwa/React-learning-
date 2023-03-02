68a3ba8282f638f06f395a2c5d2ecc93ec43d563
Damon Du <damon_du@epam.com>
2023-03-02 16:04:53 +0800

    fix: format Courses component

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-import CourseCard from './components/CourseCard/CourseCard'
+import React, { useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+import CourseCard from './components/CourseCard/CourseCard';
+import SearchBar from './components/SearchBar/SearchBar';
+import CourseInfo from '../CourseInfo/CourseInfo';
+import { queryCourseById } from '../../service/courses';
 
-import './courses.less'
+import './courses.less';
 export default function Courses() {
+  const { id } = useParams();
+
+  const result = useMemo(() => {
+    if (id) {
+      return queryCourseById(id);
+    }
+  }, [id]);
   return (
-      <CourseCard/>
-  )
+    <>
+      {id && <CourseInfo info={result} />}
+      {!id && (
+        <div>
+          <SearchBar />
+          <CourseCard />
+        </div>
+      )}
+    </>
+  );
 }
